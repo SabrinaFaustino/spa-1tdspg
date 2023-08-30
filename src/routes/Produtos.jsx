3ff61e8a23c4d1673b2c6d27c2f1cd426200ec56
SabrinaFaustino <rm99570@fiap.com.br>
2023-08-30 10:19:20 -0300

    Aula 30-08 commit

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,8 +1,43 @@
+import "./Produtos.css";
 export default function Produtos(){
     
     return(
         <main>
             <h1>Produtos</h1>
+
+            <table className="tabelaProd">
+                <tr>
+                    <th>ID</th>
+                    <th>NOME</th>
+                    <th>PRECO</th>
+                </tr>
+
+                {listaProdutos.map((produto, indice)=>(
+                    <tr key={indice}>
+                        <td>{produto.id}</td>
+                        <td>{produto.nome}</td>
+                        <td>{produto.preco}</td>
+                    </tr>
+                ))}
+            </table>
         </main>
     )
 }
+
+export const listaProdutos = [
+    {
+        id: 1,
+        nome: "Teclado",
+        preco: 150
+    },
+    {
+        id: 2,
+        nome: "Mouse",
+        preco: 120
+    },
+    {
+        id: 3,
+        nome: "Monitor",
+        preco: 950
+    }
+]
