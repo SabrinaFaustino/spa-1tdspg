4587ee5b7e5d38c69367b25193dbad6112b4c775
SabrinaFaustino <rm99570@fiap.com.br>
2023-09-06 00:05:40 -0300

    Função section

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,15 +1,15 @@
 
-export default function Section(){
-    
-    return(
+export default function Section() {
+
+    return (
         <>
             <section className="conteudo">
                 <div>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit distinctio cumque cum nobis, non perferendis et laboriosam nemo possimus itaque corrupti aliquam reiciendis optio deserunt. Aspernatur eligendi praesentium dolorem pariatur.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, illum ipsam dolor atque incidunt expedita ex adipisci, commodi nostrum quod officia voluptas, deserunt odio dicta molestiae possimus eligendi voluptates obcaecati.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit distinctio cumque cum nobis, non perferendis et laboriosam nemo possimus itaque corrupti aliquam reiciendis optio deserunt. Aspernatur eligendi praesentium dolorem pariatur.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, illum ipsam dolor atque incidunt expedita ex adipisci, commodi nostrum quod officia voluptas, deserunt odio dicta molestiae possimus eligendi voluptates obcaecati.</p>
                 </div>
             </section>
         </>
     )
-    
+
 }
