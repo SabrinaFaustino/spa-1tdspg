20ea6442f95f5ad69f66d64fe326008ef0117259
SabrinaFaustino <rm99570@fiap.com.br>
2023-09-06 00:03:23 -0300

    Adicionado rotas

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-//Bloco de criação das rotas
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Home from './routes/Home.jsx'
 import Produtos from './routes/Produtos.jsx'
